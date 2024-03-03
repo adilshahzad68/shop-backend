33934074b0b7577ee3fb2f52d724cb17de1549ed
Adil Shahzad <132524676+adilshahzad68@users.noreply.github.com>
2024-03-03 10:21:01 +0100

    cors options

--- index.js
+++ index.js
@@ -459,13 +459,22 @@ run().catch(er => console.error(er))
 
 
 
-const corsOptions = {
-  origin: 'https://shop-frontend-phi.vercel.app', // Remove the trailing slash
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+// const corsOptions = {
+//   origin: 'https://shop-frontend-phi.vercel.app', // Remove the trailing slash
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
 
-app.use(cors());
+// app.use(cors());
 
+
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 
 
